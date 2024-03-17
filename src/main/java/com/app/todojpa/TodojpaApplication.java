90484f3f5863af2e65314cdfaa4967bd0aefbeea
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-03-17 14:48:58 -0300

    feat: initial project structure created

---
+++ src/main/java/com/app/todojpa/TodojpaApplication.java
@@ -0,0 +1,13 @@
+package com.app.todojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodojpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodojpaApplication.class, args);
+	}
+
+}
